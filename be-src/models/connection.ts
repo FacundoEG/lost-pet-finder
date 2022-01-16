827f0786444ca4fe36f2111dfe3143955399b08f
FacuDEV <F_E_G.93@hotmail.com>
2022-01-16 11:15:48 -0300

    prueba 2 de env vars

--- be-src/models/connection.ts
+++ be-src/models/connection.ts
@@ -1,10 +1,9 @@
 import { Sequelize } from "sequelize";
-import { sequalizePass } from "../../keys/sequalize";
 
 export const sequelize = new Sequelize({
   dialect: "postgres",
   username: "tldpdccjfekxzm",
-  password: process.env.SEQ_PASS || sequalizePass,
+  password: process.env.SEQ_PASS,
   database: "dc1i03b0d25dvf",
   port: 5432,
   host: "ec2-18-234-17-166.compute-1.amazonaws.com",
